28c3b8e39b96dc23a4b9d995497550673b4833c6
Fisayo Afolayan <afolayanfisayo@gmail.com>
2019-04-09 16:23:23 +0200

    Update exports.sh

--- exports.sh
+++ exports.sh
@@ -1,5 +1,5 @@
 export EDITOR='subl -w'
-export HOMEBREW_GITHUB_API_TOKEN=""
+export HOMEBREW_GITHUB_API_TOKEN="insert_token_here"
 
 export PATH=/usr/local/bin:$PATH
 export PATH=$HOME/.composer/vendor/bin:$PATH
